82e7744453de53407b793f3700453922035fb29a
Stephanie Mattern <smattern416@gmail.com>
2019-08-08 09:36:00 -0400

    pseudo coded out details page and added button for shopit

--- client/src/components/Jumbotron/index.js
+++ client/src/components/Jumbotron/index.js
@@ -4,6 +4,8 @@ import "./style.css";
 function Jumbotron({ children }) {
   return <div className="jumbotron mt-4">
       <h1>ShopIt</h1>
+      <br></br>
+      <br></br>
       <p>Set your price. We ShopIt for you.</p>
   </div>;
 }
